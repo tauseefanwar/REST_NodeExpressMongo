1f702ea18512e4addbb6e6e34e64b926d95d620f
tauseef anwar <46383448+tauseefanwar@users.noreply.github.com>
2022-08-01 21:37:01 +0000

    Set up the router

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose")
+const testrouter = require("./router/routes");
 
 const app = express();
+
+app.use("/test", testrouter);
+
+app.listen(9000, () => {
+  console.log("port 9000");
+});
