2b60c8d0a05ce33137f0843189ff3a65adf04f23
tauseef anwar <46383448+tauseefanwar@users.noreply.github.com>
2022-08-02 20:44:41 +0000

    updated App.js

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const http = require("http");
-const mongoose = require("mongoose")
-const testrouter = require("./router/routes");
+const mongoose = require("mongoose");
+const alienRouter = require("./routers/routes");
 
 const app = express();
 
-app.use("/test", testrouter);
+app.use(express.json());
+
+const url =
+  "mongodb+srv://DB1:Password1!@db1.lrandit.mongodb.net/?retryWrites=true&w=majority";
+
+// DB connection
+
+const connectionParams = {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+// mongoose.connect(url,connectionParams)
+//     .then( () => {
+//         console.log('Connected to database ')
+//     })
+//     .catch( (err) => {
+//         console.error(`Error connecting to the database. \n${err}`);
+//     })
+
+mongoose.connect(url, connectionParams);
+const con = mongoose.connection;
+
+con.on("open", () => {
+  try {
+    console.log("connected...!!!");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//routing
+app.use("/aliens", alienRouter);
+const port = 9000;
 
 app.listen(9000, () => {
-  console.log("port 9000");
+  console.log(`running on ${port}`);
 });
